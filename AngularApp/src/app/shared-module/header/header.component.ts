3f255e5fd95d70c22bfc647ef49861bc7856f91e
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-09-12 20:04:19 +0200

    Komponent header i technika projekcji

--- AngularApp/src/app/shared-module/header/header.component.ts
+++ AngularApp/src/app/shared-module/header/header.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'cs-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.less']
 })
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
