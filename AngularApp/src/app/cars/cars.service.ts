bd15109982efc84499685bf13d32b1f534fb0fea
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-09-12 19:58:02 +0200

    Deklarowanie serwisów w modułach i komponentach

--- AngularApp/src/app/cars/cars.service.ts
+++ AngularApp/src/app/cars/cars.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Car } from "./models/car";
+import { Observable } from "rxjs/Observable";
+import { Http, RequestOptions, Headers } from "@angular/http";
+import 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class CarsService {
+  private apiUrl = "http://localhost:3000/api/cars";
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getCars(): Observable<Car[]> {
+    return this.http.get(this.apiUrl)
+      .map((res) => res.json())
+  }
+
+  getCar(id: number): Observable<Car> {
+    return this.http.get(this.apiUrl + `/${id}`)
+      .map((res) => res.json())
+  }
+
+  addCar(data): Observable<Car> {
+    return this.http.post(this.apiUrl, data)
+      .map((res) => res.json())
+  }
+
+  updateCar(id: number, data): Observable<Car> {
+    return this.http.put(this.apiUrl + `/${id}`, data)
+      .map((res) => res.json())
+  }
+
+  removeCar(id: number): Observable<Car> {
+    return this.http.delete(this.apiUrl + `/${id}`)
+      .map((res) => res.json())
+  }
 }
