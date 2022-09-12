b565846c2e5bfef5f6be31fcc31a8abe8358dded
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-09-12 20:10:05 +0200

    Deklarujemy pierwszy route

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarsModule } from "./cars/cars.module";
+import { CarsService } from "./cars/cars.service";
+import { CoreModule } from "./core-module/core.module";
+import { CarsListComponent } from "./cars/cars-list/cars-list.component";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
+import { CarsRoutingModule } from "./cars/cars-routing.module";
 
 @NgModule({
   declarations: [
@@ -10,9 +18,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule,
+    CarsModule,
+    CoreModule,
+    AppRoutingModule,
+    CarsRoutingModule
   ],
-  providers: [],
+  providers: [CarsService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
