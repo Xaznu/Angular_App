4c6ac44b620cf07f83efdc59c42c2346abefeeac
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-09-13 20:44:15 +0200

    Tworzymy własną dyrektywę

---
+++ AngularApp/src/app/shared-module/directives/imporant.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ImporantDirective } from './imporant.directive';
+
+describe('ImporantDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ImporantDirective();
+    expect(directive).toBeTruthy();
+  });
+});
